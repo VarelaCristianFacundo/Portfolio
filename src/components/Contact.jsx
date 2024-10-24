3e53e3bd704aaec3e36cb8905e60649bb19ee9c0
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2024-10-24 14:09:09 -0300

    email js fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,77 +1,74 @@
-import React, { useRef, useState } from "react";
-import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
+import emailjs from '@emailjs/browser'
+import { motion } from 'framer-motion'
+import React, { useRef, useState } from 'react'
 
-import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
-import { slideIn } from "../utils/motion";
+import { SectionWrapper } from '../hoc'
+import { styles } from '../styles'
+import { slideIn } from '../utils/motion'
+import { EarthCanvas } from './canvas'
 
 const Contact = () => {
-  const formRef = useRef();
+  const formRef = useRef()
   const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+    name: '',
+    email: '',
+    message: '',
+  })
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
+    const { target } = e
+    const { name, value } = target
 
     setForm({
       ...form,
       [name]: value,
-    });
-  };
+    })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    // VITE_APP_EMAILJS_SERVICE_ID=service_d3na7bi
-    // VITE_APP_EMAILJS_TEMPLATE_ID=template_5zvif2g
-    // VITE_APP_EMAILJS_PUBLIC_KEY=4Pd-fxzFnlJBfgEwl
+    e.preventDefault()
+    setLoading(true)
     emailjs
       .send(
-        "service_d3na7bi",
-        "template_5zvif2g",
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: 'Cristian',
           from_email: form.email,
-          to_email: "cvarelagarcia@gmail.com",
+          to_email: 'your_email@gmail.com',
           message: form.message,
         },
-        "4Pd-fxzFnlJBfgEwl"
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          setLoading(false)
+          alert('Thank you. I will get back to you as soon as possible.')
 
           setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
+            name: '',
+            email: '',
+            message: '',
+          })
         },
         (error) => {
-          setLoading(false);
-          console.error(error);
+          setLoading(false)
+          console.error(error)
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert('Ahh, something went wrong. Please try again.')
         }
-      );
-  };
+      )
+  }
 
   return (
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
       <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
+        variants={slideIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
@@ -120,19 +117,19 @@ const Contact = () => {
             type="submit"
             className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
-            {loading ? "Sending..." : "Send"}
+            {loading ? 'Sending...' : 'Send'}
           </button>
         </form>
       </motion.div>
 
       <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
+        variants={slideIn('right', 'tween', 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <EarthCanvas />
       </motion.div>
     </div>
-  );
-};
+  )
+}
 
-export default SectionWrapper(Contact, "contact");
+export default SectionWrapper(Contact, 'contact')
